8adc0bebe603309c64945d634bacc3402f90ea88
Jean Daniel Boccara <jdboccara@gmail.com>
2019-05-12 17:22:14 +0200

    commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController 
+{
+   /**
+     * @Route("/blog/", name="app_index")
+    */
+    public function index()
+    {
+        return $this->render('blog/default.html.twig', [ 'owner' => 'JeDi' ]);
+    }
+}
